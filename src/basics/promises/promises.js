d95051c2d625e5e05790f1b4cca6602dbfa21394
navdeyev <navdeyev@gmail.com>
2018-03-25 19:52:52 +0100

    Doing async operations with promises using ES7 async/await syntax

--- src/basics/promises/promises.js
+++ src/basics/promises/promises.js
@@ -49,3 +49,60 @@ export const lookupPopulation = (city) => {
     });
 };
 
+describe('async await syntax', () => {
+
+    it('looks up the capital for the given country', async () => {
+        expect(await lookupCapital('Germany')).toBe('Berlin');
+    });
+
+    it('returns an error message if there is no capital defined for the given country', async () => {
+        try {
+            await lookupCapital('France');
+        } catch (error) {
+            expect(error).toBe('No capital defined for France')
+        }
+    });
+
+    it('returns an array of capitals for given countries looked up in parallel', async () => {
+        const capitals = await Promise.all([
+            lookupCapital('UK'),
+            lookupCapital('Italy'),
+            lookupCapital('Germany')
+        ]);
+        expect(capitals).toEqual(['London', 'Rome', 'Berlin'])
+    });
+
+    it('returns an error message for the first failed promise', async () => {
+        const possibleErrors = [
+            'No capital defined for France',
+            'No capital defined for Portugal'
+        ];
+
+        try {
+            await Promise.all([
+                lookupCapital('UK'),
+                lookupCapital('Portugal'),
+                lookupCapital('France')
+            ]);
+        } catch (error) {
+            expect(possibleErrors.includes(error)).toBe(true);
+        }
+    });
+
+    it('returns a capital population for a given country', async () => {
+        const capital = await lookupCapital('UK');
+        const population = await lookupPopulation(capital);
+        expect(population).toBe(8787892);
+    });
+
+    it('handles the error in one single block for a chain of promises', async () => {
+        try {
+            const capital = await lookupCapital('France');
+            const population = await lookupPopulation(capital);
+        } catch (error) {
+            expect(error).toBe('No capital defined for France');
+        }
+    });
+
+});
+
